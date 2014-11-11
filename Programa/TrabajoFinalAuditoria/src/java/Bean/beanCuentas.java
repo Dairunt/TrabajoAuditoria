d2bdacf9489e2809c9ac7f2183fa9c6df81b814a
FrancoSebastian <FrancoSebastian@192.168.1.39>
2014-11-11 20:29:17 -0300

    Se agregan clases en bean, beanCuentas y interface operaciones

---
+++ Programa/TrabajoFinalAuditoria/src/java/Bean/beanCuentas.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Bean;
+
+import Dao.CuentasDao;
+import Model.Cuentas;
+import java.util.List;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author FrancoSebastian
+ */
+@Named(value = "beanCuentas")
+@ViewScoped
+public class beanCuentas implements Operaciones {
+
+    /**
+     * Creates a new instance of beanCuentas
+     */
+    List<Cuentas> cuentas;
+    Cuentas cuenta;
+    public beanCuentas() {
+       Cuentas cuenta=new Cuentas(); 
+    }
+    
+
+    @Override
+    public List Listar() {
+        CuentasDao linkDao = new CuentasDao();
+        cuentas = linkDao.Listar();
+        return cuentas;
+    }
+
+    @Override
+    public void Insertar() {
+        CuentasDao linkDao = new CuentasDao();
+        linkDao.Insertar(cuenta);
+        cuenta=new Cuentas();
+    }
+
+    @Override
+    public void Actualizar() {
+        CuentasDao linkDao = new CuentasDao();
+        linkDao.Actualizar(cuenta);
+        cuenta=new Cuentas();
+    }
+
+    @Override
+    public void Borrar() {
+        CuentasDao linkDao = new CuentasDao();
+        linkDao.Borrar(cuenta);
+        cuenta=new Cuentas();
+    }
+    
+    
+    }
+    
+
